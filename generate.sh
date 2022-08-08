27ba3928606f1711e4f67a61c250ad8161c86bda
Jibon L. Costa <jiboncosta57@gmail.com>
2022-08-08 16:09:40 +0600

    Request + Responses (#2)

--- generate.sh
+++ generate.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-protoc --proto_path=proto_files proto_files/*.proto --go_out=paths=source_relative:./plugnmeet
+protoc \
+-I ${GOPATH}/src \
+-I ${GOPATH}/pkg/mod \
+--proto_path=proto_files \
+proto_files/*.proto \
+--go_out=paths=source_relative:./plugnmeet \
+--validate_out="lang=go,paths=source_relative:./plugnmeet"
