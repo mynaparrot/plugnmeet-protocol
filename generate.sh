534e8d0446dc05796b5ede52c3c2876cb93af979
jibon Costa <jiboncosta57@gmail.com>
2022-08-08 16:08:12 +0600

    wip

--- generate.sh
+++ generate.sh
@@ -2,8 +2,7 @@
 
 protoc \
 -I ${GOPATH}/src \
--I ${GOPATH}/pkg/mod/github.com/envoyproxy/protoc-gen-validate@v0.6.7 \
--I ${GOPATH}/pkg/mod/github.com/livekit/protocol@v1.0.0 \
+-I ${GOPATH}/pkg/mod \
 --proto_path=proto_files \
 proto_files/*.proto \
 --go_out=paths=source_relative:./plugnmeet \
