96bff100930765451a72ce3772095a8eb7ca099a
jibon Costa <jiboncosta57@gmail.com>
2022-08-12 16:00:40 +0600

    dataMessage

--- plugnmeet/plugnmeet_common_api.pb.validate.go
+++ plugnmeet/plugnmeet_common_api.pb.validate.go
@@ -580,3 +580,119 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = RemoveParticipantReqValidationError{}
+
+// Validate checks the field values on DataMessageReq with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *DataMessageReq) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on DataMessageReq with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in DataMessageReqMultiError,
+// or nil if none found.
+func (m *DataMessageReq) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *DataMessageReq) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for RoomId
+
+	// no validation rules for RoomSid
+
+	// no validation rules for UserId
+
+	// no validation rules for UserSid
+
+	// no validation rules for MsgBodyType
+
+	// no validation rules for Msg
+
+	// no validation rules for RequestedUserId
+
+	// no validation rules for IsAdmin
+
+	if len(errors) > 0 {
+		return DataMessageReqMultiError(errors)
+	}
+
+	return nil
+}
+
+// DataMessageReqMultiError is an error wrapping multiple validation errors
+// returned by DataMessageReq.ValidateAll() if the designated constraints
+// aren't met.
+type DataMessageReqMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m DataMessageReqMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m DataMessageReqMultiError) AllErrors() []error { return m }
+
+// DataMessageReqValidationError is the validation error returned by
+// DataMessageReq.Validate if the designated constraints aren't met.
+type DataMessageReqValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e DataMessageReqValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e DataMessageReqValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e DataMessageReqValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e DataMessageReqValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e DataMessageReqValidationError) ErrorName() string { return "DataMessageReqValidationError" }
+
+// Error satisfies the builtin error interface
+func (e DataMessageReqValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sDataMessageReq.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = DataMessageReqValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = DataMessageReqValidationError{}
