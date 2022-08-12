bed05dc540be8656c3e2a975fff17e116bc9e45b
jibon Costa <jiboncosta57@gmail.com>
2022-08-12 14:22:52 +0600

    muteUnmuteTrack

--- plugnmeet/plugnmeet_common_api.pb.validate.go
+++ plugnmeet/plugnmeet_common_api.pb.validate.go
@@ -354,3 +354,117 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = VerifyTokenResValidationError{}
+
+// Validate checks the field values on MuteUnMuteTrackReq with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *MuteUnMuteTrackReq) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on MuteUnMuteTrackReq with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// MuteUnMuteTrackReqMultiError, or nil if none found.
+func (m *MuteUnMuteTrackReq) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *MuteUnMuteTrackReq) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Sid
+
+	// no validation rules for RoomId
+
+	// no validation rules for UserId
+
+	// no validation rules for TrackSid
+
+	// no validation rules for Muted
+
+	// no validation rules for RequestedUserId
+
+	if len(errors) > 0 {
+		return MuteUnMuteTrackReqMultiError(errors)
+	}
+
+	return nil
+}
+
+// MuteUnMuteTrackReqMultiError is an error wrapping multiple validation errors
+// returned by MuteUnMuteTrackReq.ValidateAll() if the designated constraints
+// aren't met.
+type MuteUnMuteTrackReqMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m MuteUnMuteTrackReqMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m MuteUnMuteTrackReqMultiError) AllErrors() []error { return m }
+
+// MuteUnMuteTrackReqValidationError is the validation error returned by
+// MuteUnMuteTrackReq.Validate if the designated constraints aren't met.
+type MuteUnMuteTrackReqValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e MuteUnMuteTrackReqValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e MuteUnMuteTrackReqValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e MuteUnMuteTrackReqValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e MuteUnMuteTrackReqValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e MuteUnMuteTrackReqValidationError) ErrorName() string {
+	return "MuteUnMuteTrackReqValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e MuteUnMuteTrackReqValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sMuteUnMuteTrackReq.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = MuteUnMuteTrackReqValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = MuteUnMuteTrackReqValidationError{}
