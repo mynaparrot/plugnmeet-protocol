3783e5384959212ddce96e522b1fe92341fe2514
Jibon L. Costa <jiboncosta57@gmail.com>
2022-09-24 12:01:11 +0600

    option added `WHITEBOARD_APP_STATE_CHANGE`

--- generate.sh
+++ generate.sh
@@ -3,8 +3,8 @@
 protoc \
 -I ${GOPATH}/src \
 -I ${GOPATH}/pkg/mod \
--I ${GOPATH}/pkg/mod/github.com/livekit/protocol@v1.0.0 \
--I ${GOPATH}/pkg/mod/github.com/envoyproxy/protoc-gen-validate@v0.6.7 \
+-I ${GOPATH}/pkg/mod/github.com/livekit/protocol@v1.1.2 \
+-I ${GOPATH}/pkg/mod/github.com/envoyproxy/protoc-gen-validate@v0.6.8 \
 --proto_path=proto_files \
 proto_files/*.proto \
 --go_out=paths=source_relative:./plugnmeet \
