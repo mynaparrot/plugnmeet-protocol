7e3c589f404e690288e96d6e5835b9cecda4df75
Jibon L. Costa <jiboncosta57@gmail.com>
2023-01-02 10:47:05 +0100

    dependencies update

--- generate.sh
+++ generate.sh
@@ -3,8 +3,8 @@
 protoc \
 -I ${GOPATH}/src \
 -I ${GOPATH}/pkg/mod \
--I ${GOPATH}/pkg/mod/github.com/livekit/protocol@v1.2.2 \
--I ${GOPATH}/pkg/mod/github.com/envoyproxy/protoc-gen-validate@v0.9.0 \
+-I ${GOPATH}/pkg/mod/github.com/livekit/protocol@v1.3.1 \
+-I ${GOPATH}/pkg/mod/github.com/envoyproxy/protoc-gen-validate@v0.9.1 \
 --proto_path=proto_files \
 proto_files/*.proto \
 --go_out=paths=source_relative:./plugnmeet \
