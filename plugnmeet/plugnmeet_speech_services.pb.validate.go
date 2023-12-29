9d1475326a405d1f6b45e9f5271681146931c592
Jibon <jiboncosta57@gmail.com>
2023-12-29 15:03:31 +0100

    renew token

--- plugnmeet/plugnmeet_speech_services.pb.validate.go
+++ plugnmeet/plugnmeet_speech_services.pb.validate.go
@@ -279,6 +279,8 @@ func (m *GenerateAzureTokenRes) validate(all bool) error {
 
 	// no validation rules for Msg
 
+	// no validation rules for Renew
+
 	if m.Token != nil {
 		// no validation rules for Token
 	}
@@ -370,6 +372,116 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = GenerateAzureTokenResValidationError{}
+
+// Validate checks the field values on AzureTokenRenewReq with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *AzureTokenRenewReq) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on AzureTokenRenewReq with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// AzureTokenRenewReqMultiError, or nil if none found.
+func (m *AzureTokenRenewReq) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *AzureTokenRenewReq) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for RoomId
+
+	// no validation rules for UserSid
+
+	// no validation rules for ServiceRegion
+
+	// no validation rules for KeyId
+
+	if len(errors) > 0 {
+		return AzureTokenRenewReqMultiError(errors)
+	}
+
+	return nil
+}
+
+// AzureTokenRenewReqMultiError is an error wrapping multiple validation errors
+// returned by AzureTokenRenewReq.ValidateAll() if the designated constraints
+// aren't met.
+type AzureTokenRenewReqMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m AzureTokenRenewReqMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m AzureTokenRenewReqMultiError) AllErrors() []error { return m }
+
+// AzureTokenRenewReqValidationError is the validation error returned by
+// AzureTokenRenewReq.Validate if the designated constraints aren't met.
+type AzureTokenRenewReqValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e AzureTokenRenewReqValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e AzureTokenRenewReqValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e AzureTokenRenewReqValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e AzureTokenRenewReqValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e AzureTokenRenewReqValidationError) ErrorName() string {
+	return "AzureTokenRenewReqValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e AzureTokenRenewReqValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sAzureTokenRenewReq.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = AzureTokenRenewReqValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = AzureTokenRenewReqValidationError{}
 
 // Validate checks the field values on SpeechServiceUserStatusReq with the
 // rules defined in the proto definition for this message. If any rules are
