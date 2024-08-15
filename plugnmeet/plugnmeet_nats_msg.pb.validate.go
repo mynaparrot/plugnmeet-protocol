11cd735cd40f257a5286989ba5a488f157c68746
Jibon <jiboncosta57@gmail.com>
2024-08-15 18:37:39 +0200

    wip

--- plugnmeet/plugnmeet_nats_msg.pb.validate.go
+++ plugnmeet/plugnmeet_nats_msg.pb.validate.go
@@ -250,3 +250,219 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = NatsMsgClientToServerValidationError{}
+
+// Validate checks the field values on NatsKvRoomInfo with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *NatsKvRoomInfo) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on NatsKvRoomInfo with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in NatsKvRoomInfoMultiError,
+// or nil if none found.
+func (m *NatsKvRoomInfo) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *NatsKvRoomInfo) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for RoomId
+
+	// no validation rules for RoomSid
+
+	// no validation rules for Metadata
+
+	if len(errors) > 0 {
+		return NatsKvRoomInfoMultiError(errors)
+	}
+
+	return nil
+}
+
+// NatsKvRoomInfoMultiError is an error wrapping multiple validation errors
+// returned by NatsKvRoomInfo.ValidateAll() if the designated constraints
+// aren't met.
+type NatsKvRoomInfoMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m NatsKvRoomInfoMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m NatsKvRoomInfoMultiError) AllErrors() []error { return m }
+
+// NatsKvRoomInfoValidationError is the validation error returned by
+// NatsKvRoomInfo.Validate if the designated constraints aren't met.
+type NatsKvRoomInfoValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e NatsKvRoomInfoValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e NatsKvRoomInfoValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e NatsKvRoomInfoValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e NatsKvRoomInfoValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e NatsKvRoomInfoValidationError) ErrorName() string { return "NatsKvRoomInfoValidationError" }
+
+// Error satisfies the builtin error interface
+func (e NatsKvRoomInfoValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sNatsKvRoomInfo.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = NatsKvRoomInfoValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = NatsKvRoomInfoValidationError{}
+
+// Validate checks the field values on NatsKvUserInfo with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *NatsKvUserInfo) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on NatsKvUserInfo with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in NatsKvUserInfoMultiError,
+// or nil if none found.
+func (m *NatsKvUserInfo) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *NatsKvUserInfo) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for UserId
+
+	// no validation rules for UserSid
+
+	// no validation rules for Name
+
+	// no validation rules for RoomId
+
+	// no validation rules for Metadata
+
+	if len(errors) > 0 {
+		return NatsKvUserInfoMultiError(errors)
+	}
+
+	return nil
+}
+
+// NatsKvUserInfoMultiError is an error wrapping multiple validation errors
+// returned by NatsKvUserInfo.ValidateAll() if the designated constraints
+// aren't met.
+type NatsKvUserInfoMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m NatsKvUserInfoMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m NatsKvUserInfoMultiError) AllErrors() []error { return m }
+
+// NatsKvUserInfoValidationError is the validation error returned by
+// NatsKvUserInfo.Validate if the designated constraints aren't met.
+type NatsKvUserInfoValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e NatsKvUserInfoValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e NatsKvUserInfoValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e NatsKvUserInfoValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e NatsKvUserInfoValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e NatsKvUserInfoValidationError) ErrorName() string { return "NatsKvUserInfoValidationError" }
+
+// Error satisfies the builtin error interface
+func (e NatsKvUserInfoValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sNatsKvUserInfo.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = NatsKvUserInfoValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = NatsKvUserInfoValidationError{}
