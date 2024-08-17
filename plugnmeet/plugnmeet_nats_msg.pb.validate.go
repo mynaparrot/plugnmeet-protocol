e24f137dd21fc7328933f0a6cef7b450fe990328
Jibon L. Costa <jiboncosta57@gmail.com>
2024-08-17 16:15:00 +0200

    wip

--- plugnmeet/plugnmeet_nats_msg.pb.validate.go
+++ plugnmeet/plugnmeet_nats_msg.pb.validate.go
@@ -588,3 +588,113 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = MediaServerConnInfoValidationError{}
+
+// Validate checks the field values on NatsSystemNotification with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *NatsSystemNotification) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on NatsSystemNotification with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// NatsSystemNotificationMultiError, or nil if none found.
+func (m *NatsSystemNotification) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *NatsSystemNotification) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Id
+
+	// no validation rules for Type
+
+	// no validation rules for Msg
+
+	// no validation rules for SentAt
+
+	if len(errors) > 0 {
+		return NatsSystemNotificationMultiError(errors)
+	}
+
+	return nil
+}
+
+// NatsSystemNotificationMultiError is an error wrapping multiple validation
+// errors returned by NatsSystemNotification.ValidateAll() if the designated
+// constraints aren't met.
+type NatsSystemNotificationMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m NatsSystemNotificationMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m NatsSystemNotificationMultiError) AllErrors() []error { return m }
+
+// NatsSystemNotificationValidationError is the validation error returned by
+// NatsSystemNotification.Validate if the designated constraints aren't met.
+type NatsSystemNotificationValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e NatsSystemNotificationValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e NatsSystemNotificationValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e NatsSystemNotificationValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e NatsSystemNotificationValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e NatsSystemNotificationValidationError) ErrorName() string {
+	return "NatsSystemNotificationValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e NatsSystemNotificationValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sNatsSystemNotification.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = NatsSystemNotificationValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = NatsSystemNotificationValidationError{}
