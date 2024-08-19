92be372465dc13910560d9e5ebe197a3c7f63860
Jibon <jiboncosta57@gmail.com>
2024-08-19 14:58:31 +0200

    :)

--- plugnmeet/plugnmeet_nats_msg.pb.validate.go
+++ plugnmeet/plugnmeet_nats_msg.pb.validate.go
@@ -885,3 +885,109 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = NatsSystemNotificationValidationError{}
+
+// Validate checks the field values on NatsUserMetadataUpdate with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *NatsUserMetadataUpdate) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on NatsUserMetadataUpdate with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// NatsUserMetadataUpdateMultiError, or nil if none found.
+func (m *NatsUserMetadataUpdate) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *NatsUserMetadataUpdate) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for UserId
+
+	// no validation rules for Metadata
+
+	if len(errors) > 0 {
+		return NatsUserMetadataUpdateMultiError(errors)
+	}
+
+	return nil
+}
+
+// NatsUserMetadataUpdateMultiError is an error wrapping multiple validation
+// errors returned by NatsUserMetadataUpdate.ValidateAll() if the designated
+// constraints aren't met.
+type NatsUserMetadataUpdateMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m NatsUserMetadataUpdateMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m NatsUserMetadataUpdateMultiError) AllErrors() []error { return m }
+
+// NatsUserMetadataUpdateValidationError is the validation error returned by
+// NatsUserMetadataUpdate.Validate if the designated constraints aren't met.
+type NatsUserMetadataUpdateValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e NatsUserMetadataUpdateValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e NatsUserMetadataUpdateValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e NatsUserMetadataUpdateValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e NatsUserMetadataUpdateValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e NatsUserMetadataUpdateValidationError) ErrorName() string {
+	return "NatsUserMetadataUpdateValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e NatsUserMetadataUpdateValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sNatsUserMetadataUpdate.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = NatsUserMetadataUpdateValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = NatsUserMetadataUpdateValidationError{}
