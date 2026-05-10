aee556bb5bb4dc3211e98b32dbb224c824df5cfc
Jibon L. Costa <jiboncosta57@gmail.com>
2026-05-10 18:56:31 +0200

    fix: upgrade to fiber v3

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/DeRuina/timberjack v1.4.2
 	github.com/gammazero/workerpool v1.2.1
 	github.com/go-jose/go-jose/v4 v4.1.4
-	github.com/gofiber/fiber/v2 v2.52.13
+	github.com/gofiber/fiber/v3 v3.2.0
 	github.com/google/uuid v1.6.0
 	github.com/hashicorp/go-retryablehttp v0.7.8
 	github.com/livekit/protocol v1.45.8
@@ -25,7 +25,6 @@ require (
 	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
 	github.com/benbjohnson/clock v1.3.5 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/dennwc/iters v1.2.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/frostbyte73/core v0.1.1 // indirect
@@ -33,6 +32,8 @@ require (
 	github.com/gammazero/deque v1.2.1 // indirect
 	github.com/go-jose/go-jose/v3 v3.0.5 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/gofiber/schema v1.7.1 // indirect
+	github.com/gofiber/utils/v2 v2.0.4 // indirect
 	github.com/google/cel-go v0.27.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/jxskiss/base62 v1.1.0 // indirect
@@ -43,8 +44,8 @@ require (
 	github.com/livekit/psrpc v0.7.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.22 // indirect
-	github.com/mattn/go-runewidth v0.0.23 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
 	github.com/pion/datachannel v1.6.0 // indirect
 	github.com/pion/dtls/v3 v3.1.2 // indirect
 	github.com/pion/ice/v4 v4.2.5 // indirect
@@ -63,9 +64,10 @@ require (
 	github.com/pion/webrtc/v4 v4.2.12 // indirect
 	github.com/puzpuzpuz/xsync/v3 v3.5.1 // indirect
 	github.com/redis/go-redis/v9 v9.18.0 // indirect
+	github.com/tinylib/msgp v1.6.4 // indirect
 	github.com/twitchtv/twirp v8.1.3+incompatible // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.69.0 // indirect
+	github.com/valyala/fasthttp v1.70.0 // indirect
 	github.com/wlynxg/anet v0.0.5 // indirect
 	github.com/zeebo/xxh3 v1.1.0 // indirect
 	go.opentelemetry.io/otel v1.43.0 // indirect
--- utils/common.go
+++ utils/common.go
@@ -13,7 +13,7 @@ import (
 	"time"
 	"unicode"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v3"
 	"github.com/livekit/protocol/livekit"
 	"github.com/mynaparrot/plugnmeet-protocol/plugnmeet"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -26,7 +26,6 @@ var op = protojson.MarshalOptions{
 }
 
 func PrepareCommonWebhookNotifyEvent(event *livekit.WebhookEvent) *plugnmeet.CommonNotifyEvent {
-	ct := uint64(event.Room.CreationTime)
 	return &plugnmeet.CommonNotifyEvent{
 		Event: &event.Event,
 		Room: &plugnmeet.NotifyEventRoom{
@@ -34,7 +33,7 @@ func PrepareCommonWebhookNotifyEvent(event *livekit.WebhookEvent) *plugnmeet.Com
 			RoomId:          &event.Room.Name,
 			EmptyTimeout:    &event.Room.EmptyTimeout,
 			MaxParticipants: &event.Room.MaxParticipants,
-			CreationTime:    &ct,
+			CreationTime:    new(uint64(event.Room.CreationTime)),
 			EnabledCodecs:   event.Room.EnabledCodecs,
 			Metadata:        &event.Room.Metadata,
 			NumParticipants: &event.Room.NumParticipants,
@@ -46,7 +45,7 @@ func PrepareCommonWebhookNotifyEvent(event *livekit.WebhookEvent) *plugnmeet.Com
 	}
 }
 
-func SendCommonProtobufResponse(c *fiber.Ctx, s bool, m string) error {
+func SendCommonProtobufResponse(c fiber.Ctx, s bool, m string) error {
 	res := &plugnmeet.CommonResponse{
 		Status: s,
 		Msg:    m,
@@ -59,7 +58,7 @@ func SendCommonProtobufResponse(c *fiber.Ctx, s bool, m string) error {
 	return c.Send(marshal)
 }
 
-func SendProtobufResponse(c *fiber.Ctx, res proto.Message) error {
+func SendProtobufResponse(c fiber.Ctx, res proto.Message) error {
 	marshal, err := proto.Marshal(res)
 	if err != nil {
 		return err
@@ -68,7 +67,7 @@ func SendProtobufResponse(c *fiber.Ctx, res proto.Message) error {
 	return c.Send(marshal)
 }
 
-func SendCommonProtoJsonResponse(c *fiber.Ctx, s bool, m string, statusCode plugnmeet.StatusCode) error {
+func SendCommonProtoJsonResponse(c fiber.Ctx, s bool, m string, statusCode plugnmeet.StatusCode) error {
 	res := &plugnmeet.CommonResponse{
 		Status:     s,
 		Msg:        m,
@@ -82,7 +81,7 @@ func SendCommonProtoJsonResponse(c *fiber.Ctx, s bool, m string, statusCode plug
 	return c.Send(marshal)
 }
 
-func SendProtoJsonResponse(c *fiber.Ctx, res proto.Message) error {
+func SendProtoJsonResponse(c fiber.Ctx, res proto.Message) error {
 	marshal, err := op.Marshal(res)
 	if err != nil {
 		return err
